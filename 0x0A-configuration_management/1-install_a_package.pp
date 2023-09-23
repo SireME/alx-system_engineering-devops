ffe6ee0639eab4224dd3c5d284fe193009808f65
Mbang Ernest N <mbangernest@yahoo.com>
2023-09-23 03:57:52 +0100

    create file a specific loaction

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,8 @@
+# file creation at specific location
+
+file { '/tmp/school':
+  content =>'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,2 @@
+# 0x0A-configuration_management  
+This directory contains into to confguration managment using puppet
